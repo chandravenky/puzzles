73766c616373c7d95be49adf9b58be31ed63d90c
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-12-22 09:18:20 -0700

    function in class

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -41,3 +41,34 @@ def test_roman_to_integer():
   return output1, output2, output3
 
 print(test_roman_to_integer())
+
+#For leetcode
+class Solution(object):
+    def romanToInt(self, input_roman):
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+
+        output =0
+        i =0
+
+        while i< len(input_roman):
+
+            if i < len(input_roman)-1 and roman_values[input_roman[i]] < roman_values[input_roman[i+1]]:
+                output =  output + roman_values[input_roman[i+1]] - roman_values[input_roman[i]]
+
+                i = i + 2
+
+            else:
+
+                output = output + roman_values[input_roman[i]]
+                i= i+1
+
+        return output
+        
