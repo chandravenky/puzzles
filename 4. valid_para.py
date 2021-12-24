ed1f078cc74b5a379a4cc99b26e52b40b684fbee
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-12-24 08:51:07 -0700

    formal code

--- 4. valid_para.py
+++ 4. valid_para.py
@@ -58,3 +58,41 @@ def test_valid_parant():
   return output1, output2, output3, output4, output5, output6, output7, output8
 
 print(test_valid_parant())
+
+#For leetcode
+class Solution(object):
+    def isValid(self, input_string):
+        open_close_map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+        }
+
+        open_close_array=[]
+
+        #odd can simply be removed
+        if len(input_string)%2 !=0 or len(input_string)==0:
+            return False
+
+        for i in range(0, len(input_string)):
+
+            #if something gets opened, it is stored in order
+            if input_string[i] in open_close_map.keys():
+                open_close_array.append(input_string[i])
+
+            #if a closing is encountered, it must follow order
+            elif input_string[i] in open_close_map.values() and open_close_array:
+                if input_string[i] != open_close_map[open_close_array.pop()]:
+                    return False
+
+            #if only closing is encountered, return False
+            else:
+                return False
+
+        #if something remains unclosed, return False
+        if open_close_array:
+            return False
+
+        #if everything is closed, return True
+        return True
+
