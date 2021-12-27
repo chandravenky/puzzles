da06a2f7c8e0154faf9d76fc399ce27d25a4544f
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-12-27 13:42:25 -0700

    solution2

--- 7. search_and_insert.py
+++ 7. search_and_insert.py
@@ -10,3 +10,29 @@ def search_and_insert(nums, target):
       return i
     
   return i+1
+
+#Solution 2
+def search_and_insert2(nums, target):
+  start_index = 0
+  half_index = len(nums)//2
+  end_index = len(nums)
+
+  while True:
+
+      if target < nums[half_index]:
+          end_index = half_index
+          half_index = (start_index + half_index)//2
+
+      elif target > nums[half_index]:
+          start_index = half_index
+          half_index = (half_index + end_index)//2
+
+      if target == nums[half_index]:
+          return half_index
+
+      if (half_index == end_index or half_index == start_index) and target>nums[half_index]:
+
+          return half_index +1
+
+      if (half_index == end_index or half_index == start_index)  and target<nums[half_index]:
+          return half_index
