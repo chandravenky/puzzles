2be78e4ed13293f14221be444867347b8b9a22c6
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-12-30 09:30:21 -0700

    solution2

--- 10. plus one.py
+++ 10. plus one.py
@@ -8,3 +8,16 @@ def plus_one(input_array):
   split_form = [int(item) for item in str(incremented_num)]
     
   return split_form
+
+#Solution2
+def plus_one_2(input_array):
+  
+  num = 0
+
+  for i in range(0, len(input_array)):
+
+    num = num + input_array[i]*(10**(len(input_array) - i-1))
+
+  num = num +1
+
+  return list(str(num))
