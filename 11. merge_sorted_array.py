b068af0b0eeda06d6f2dedd5f1daf9fe2ec63daf
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-12-31 08:22:58 -0700

    add tests

--- 11. merge_sorted_array.py
+++ 11. merge_sorted_array.py
@@ -39,3 +39,26 @@ def merge_sorted_array(nums1, m, nums2, n):
 
 
   return nums1
+
+#Tests
+def merge_sorted_array_test():
+
+  actual_output1 = merge_sorted_array([1,2,3,0,0,0], 3, [2,5,6], 3)
+  actual_output2 = merge_sorted_array([1], 1, [], 0)
+  actual_output3 = merge_sorted_array([0], 0, [1], 1)
+  actual_output4 = merge_sorted_array([2,0], 1, [1], 1)
+  actual_output5 = merge_sorted_array([1,2,3,0,0,0], 3, [4,5,6], 3)
+  actual_output6 = merge_sorted_array([4,5,6,0,0,0], 3, [1,2,3], 3)
+  actual_output7 = merge_sorted_array([1,2,3,4,5], 5, [], 0)
+
+  expected_output1 = [1,2,2,3,5,6]
+  expected_output2 = [1]
+  expected_output3 = [1]
+  expected_output4 = [1,2]
+  expected_output5 = [1,2,3,4,5,6]
+  expected_output6 = [1,2,3,4,5,6]
+  expected_output7 = [1,2,3,4,5]
+
+  return actual_output1 == expected_output1, actual_output2 == expected_output2, actual_output3 == expected_output3, actual_output4 == expected_output4, actual_output5 == expected_output5, actual_output6 == expected_output6, actual_output7 == expected_output7 
+
+print(merge_sorted_array_test())
