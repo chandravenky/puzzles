3c09860cb06cd0bff5fdeda363e6188ecc181f08
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-01 09:21:44 -0700

    second solution

--- 12. single_number.py
+++ 12. single_number.py
@@ -14,3 +14,18 @@ def single_number(input_array):
       unique_track[input_array[i]] = 1
 
   return list(unique_track.keys())[0]
+
+#Solution 2
+def single_number_2(input_array):
+
+  res = input_array[0]
+
+  for i in range(1, len(input_array)):
+
+    #print(res, bin(res), input_array[i], bin(input_array[i]))
+
+    res ^= input_array[i] #bitwise XOR
+
+    #print("result", res, bin(res))
+
+  return res
