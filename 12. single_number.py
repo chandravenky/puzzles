0ee055a94d729ffeb1f213cae437120600e92133
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-01 09:22:13 -0700

    both the solutions

--- 12. single_number.py
+++ 12. single_number.py
@@ -49,3 +49,43 @@ def single_number_2_test():
 
 print(single_number_test())
 print(single_number_2_test())
+
+#For Leetcode
+#Solution1
+class Solution(object):
+    def singleNumber(self, input_array):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        unique_track = {}
+
+        for i in range(0,len(input_array)):
+
+            if input_array[i] in unique_track:
+
+                del unique_track[input_array[i]]
+
+            else:
+                unique_track[input_array[i]] = 1
+
+        return list(unique_track.keys())[0]
+
+#Solution2
+class Solution(object):
+    def singleNumber(self, input_array):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = input_array[0]
+
+        for i in range(1, len(input_array)):
+
+            #print(res, bin(res), input_array[i], bin(input_array[i]))
+
+            res ^= input_array[i] #bitwise XOR
+
+            #print("result", res, bin(res))
+
+        return res
