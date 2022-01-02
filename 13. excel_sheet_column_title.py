6b2464eee5bc9c962b366e320ba9b521a4f97a44
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-02 08:26:24 -0700

    solution 1

---
+++ 13. excel_sheet_column_title.py
@@ -0,0 +1,23 @@
+#Less readable - uses original index
+def excel_sheet_column_num_2(input_col_num):
+
+  map_dict = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  result = ''
+
+  if input_col_num <=26:
+    return map_dict[input_col_num-1]
+
+  else:
+
+    while input_col_num>0:
+
+      input_col_num, remainder = divmod(input_col_num,26)
+      
+      if remainder == 0:
+
+        remainder = 26
+        input_col_num = input_col_num -1
+      
+      result = map_dict[remainder-1] + result
+
+  return result
