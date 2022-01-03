71de2209a279364fb98f902b3d414f5fd1d65c09
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-03 09:21:54 -0700

    boyer moore

--- 14. majority_element.py
+++ 14. majority_element.py
@@ -59,3 +59,53 @@ def majority_element_test():
 
 
 print(majority_element_test())
+
+#Leetcode
+#Solution1
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        threshold = len(nums)/2
+
+        cnt_dict = {}
+        
+        if len(nums) == 1:
+            return nums[0]
+
+        for num in nums:
+
+            if num in cnt_dict:
+                cnt_dict[num] = cnt_dict[num] + 1
+                if cnt_dict[num]>threshold:
+                    return num
+
+            else:
+                cnt_dict[num] = 1
+
+#Solution2
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        res = nums[0]
+
+        for num in nums[1:]:
+
+            if num != res:
+
+                count = count-1
+
+                if count <0:
+                    count = 0
+                    res = num
+
+            else:
+                count = count + 1
+
+        return res
