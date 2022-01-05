bd64917c9b28fe1561f8e4ccb497867417863c78
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-05 08:33:42 -0700

    class solution

--- 16. excel_sheet_column_number.py
+++ 16. excel_sheet_column_number.py
@@ -29,3 +29,24 @@ def excel_sheet_col_num_test():
   return ( excel_sheet_col_num(input_col_name1) == expected_output1, excel_sheet_col_num(input_col_name2) == expected_output2, excel_sheet_col_num(input_col_name3) == expected_output3, excel_sheet_col_num(input_col_name4) == expected_output4, excel_sheet_col_num(input_col_name5) == expected_output5 )
 
 print(excel_sheet_col_num_test())
+
+
+#Submission
+class Solution(object):
+  def titleToNumber(self, input_col_name):
+    """
+    :type columnTitle: str
+    :rtype: int
+    """
+    input_col_name = input_col_name.lower()
+
+    num = 0
+
+    for i in range(0, len(input_col_name)):
+
+        # print(i, input_col_name[i], ord(input_col_name[i])-96)
+
+        num = num + ((26** (len(input_col_name)-i-1))*(ord(input_col_name[i])-96))
+
+    return num
+    
