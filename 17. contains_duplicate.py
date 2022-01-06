fe8d45eb28119dcd811160ea29736fffc3a493ba
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-06 09:47:04 -0700

    add tests

--- 17. contains_duplicate.py
+++ 17. contains_duplicate.py
@@ -12,3 +12,16 @@ def contains_duplicate(input_array):
       map_dict[input_array[i]] = 1
 
   return False
+
+#Tests
+def contains_duplicate_test():
+
+  input_array1 = [1,2,3,1]
+  input_array2 = [1,2,3,4]
+  input_array3 = [1,1,1,3,3,4,3,2,4,2]
+  input_array4 = [1]
+  input_array5 = []
+
+  return ( contains_duplicate(input_array1) == True, contains_duplicate(input_array2) == False, contains_duplicate(input_array3) == True, contains_duplicate(input_array4) == False, contains_duplicate(input_array5) == False )
+
+print(contains_duplicate_test())
