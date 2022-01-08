c416c0b0fc421445c1fc5e816e3e0743c247132a
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-08 09:35:19 -0700

    add in class solution

--- 20. valid_anagram.py
+++ 20. valid_anagram.py
@@ -38,3 +38,40 @@ def is_anagram_test():
   return ( is_anagram(input_case1[0], input_case1[1]) == True, is_anagram(input_case2[0], input_case2[1]) == False, is_anagram(input_case3[0], input_case3[1]) == False)
 
 print(is_anagram_test())
+
+#Leetcode
+class Solution(object):
+  def isAnagram(self, s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    s_count = {}
+    
+    if len(s) != len(t):
+      return False
+
+    for letter in s:
+
+      if letter in s_count:
+        s_count[letter] = s_count[letter] + 1
+
+      else:
+        s_count[letter] = 1
+
+    for letter in t:
+
+      if letter in s_count:
+        s_count[letter] = s_count[letter] -1
+
+        if s_count[letter] == 0:
+          del s_count[letter]
+
+        else:
+          return False
+    if s_count:
+      return False
+
+    return True
+
