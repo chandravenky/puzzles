3dbab2918c8cd681d1c98f24d4ceab76d2a5c91d
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-09 09:42:32 -0700

    add in class

--- 19. power_of_two.py
+++ 19. power_of_two.py
@@ -29,3 +29,27 @@ def power_of_two_test():
   return ( power_of_two(input_number1) == True, power_of_two(input_number2) == True, power_of_two(input_number3) == False, power_of_two(input_number4) == False, power_of_two(input_number5) == True )
 
 print(power_of_two_test())
+
+#Leetcode
+class Solution(object):
+  def isPowerOfTwo(self, input_number):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    if input_number == 1 or input_number==2:
+      return True
+
+    if input_number == 0:
+      return False
+
+    while input_number>=2:
+
+      #leetcode evaluates in Python 2.7
+      input_number = float(input_number)/float(2)
+
+      if input_number ==2:
+
+        return True
+
+    return False
