df6d24ef1d73f364bf063f0514f6e943ec0bdc7f
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-09 09:42:22 -0700

    add test

--- 19. power_of_two.py
+++ 19. power_of_two.py
@@ -16,3 +16,16 @@ def power_of_two(input_number):
       return True
 
   return False
+
+# Tests
+def power_of_two_test():
+
+  input_number1 = 1
+  input_number2 = 16
+  input_number3 = 17
+  input_number4 = 0
+  input_number5 = 2
+
+  return ( power_of_two(input_number1) == True, power_of_two(input_number2) == True, power_of_two(input_number3) == False, power_of_two(input_number4) == False, power_of_two(input_number5) == True )
+
+print(power_of_two_test())
