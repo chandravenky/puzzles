22f456b9c562a5644cc4d751177d4798469849e1
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-10 09:12:36 -0700

    with recursion

--- 21. add_digits.py
+++ 21. add_digits.py
@@ -14,3 +14,18 @@ def add_digits(input_number):
       res = res + int(digit)
 
   return res
+
+#With recursion
+def add_digits_recursively(input_number):
+
+  res = 0
+    
+  for digit in str(input_number):
+
+    res = res + int(digit)
+  
+  if len(str(res)) > 1:
+
+    return add_digits_recursively(res)
+    
+  return res
