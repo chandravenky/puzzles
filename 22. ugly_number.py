2ef757563e598d270ee98c7dc7813f3462536f59
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-11 08:48:21 -0700

    add tests

--- 22. ugly_number.py
+++ 22. ugly_number.py
@@ -21,3 +21,24 @@ def ugly_number(input_number):
       return False
 
   return True
+
+# Tests
+def ugly_number_test():
+
+  input_number1 = 6
+  input_number2 = 1
+  input_number3 = 14
+  input_number4 = 8
+  input_number5 = 0
+
+
+  expected_output1 = True
+  expected_output2 = True
+  expected_output3 = False
+  expected_output4 = True
+  expected_output5 = False
+
+
+  return ugly_number(input_number1) == expected_output1, ugly_number(input_number2) == expected_output2, ugly_number(input_number3) == expected_output3, ugly_number(input_number4) == expected_output4, ugly_number(input_number5) == expected_output5
+
+print(ugly_number_test())
