e01d892459b6a3140f255eb166948e67aca96bc4
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-12 09:35:10 -0700

    add tests

--- 23. move_zeroes.py
+++ 23. move_zeroes.py
@@ -20,3 +20,24 @@ def move_zeroes(input_array):
       pass
     
   return input_array
+
+# Tests
+def move_zeroes_test():
+
+  input_array1 = [0,1,0,3,12]
+  input_array2 = [0]
+  input_array3 = [2,1]
+  input_array4 = [0,8]
+  input_array5 = [1,0,0,4]
+
+
+  expected_output1 = [1,3,12,0,0]
+  expected_output2 = [0]
+  expected_output3 = [2,1]
+  expected_output4 = [8,0]
+  expected_output5 = [1,4,0,0]
+
+
+  return move_zeroes(input_array1) == expected_output1, move_zeroes(input_array2) == expected_output2, move_zeroes(input_array3) == expected_output3, move_zeroes(input_array4) == expected_output4, move_zeroes(input_array5) == expected_output5
+
+print(move_zeroes_test())
