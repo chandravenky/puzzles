691f9dd60f0a2d6539a9c48009cafaf757c8fc5f
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-13 08:40:55 -0700

    add tests

--- 24. missing_number.py
+++ 24. missing_number.py
@@ -20,3 +20,23 @@ def missing_number2(input_array):
         tot_sum = tot_sum -i
         
     return tot_sum
+
+#Tests
+def missing_number_test():
+
+  input_array1 = [9,6,4,2,3,5,7,0,1]
+  input_array2 = [0,1,3]
+  input_array3 = [0,1]
+
+
+  expected_output1 = 8
+  expected_output2 = 2
+  expected_output3 = 2
+
+  result_tuple1 = ( expected_output1 == missing_number(input_array1), expected_output2 == missing_number(input_array2), expected_output3 == missing_number(input_array3) )
+
+  result_tuple2 = ( expected_output1 == missing_number2(input_array1), expected_output2 == missing_number2(input_array2), expected_output3 == missing_number2(input_array3) )
+
+  return result_tuple1, result_tuple2
+
+print(missing_number_test())
