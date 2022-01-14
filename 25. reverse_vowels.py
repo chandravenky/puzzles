a38b60bfd6fb3b2021fb9153e8e9ad4fa4c87da1
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-14 08:58:33 -0700

    final in class

--- 25. reverse_vowels.py
+++ 25. reverse_vowels.py
@@ -40,3 +40,32 @@ def reverse_vowels_test():
   return ( expected_output1 == reverse_vowels(input_string1), expected_output2 == reverse_vowels(input_string2), expected_output3 == reverse_vowels(input_string3), expected_output4 == reverse_vowels(input_string4) )
 
 print(reverse_vowels_test())
+
+#Leetcode
+class Solution(object):
+  def reverseVowels(self, input_string):
+    """
+    :type s: str
+    :rtype: str
+    """
+    vowel_string = ''
+    index_string = []
+    input_string = list(input_string)
+
+    for index, letter in enumerate(input_string):
+
+      if letter.lower() in ['a', 'e', 'i', 'o', 'u']:
+
+        vowel_string = vowel_string + letter
+
+        index_string.append(index)
+
+    #Reverse the vowel string
+    vowel_string = list(vowel_string[::-1])
+
+    for i in range(0, len(index_string)):
+
+      input_string[index_string[i]] = vowel_string[i]
+
+    return ''.join(input_string)
+
