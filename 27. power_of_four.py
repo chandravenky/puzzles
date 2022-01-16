c95497a1ab210273ee93969846b90d3cb4358390
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-16 09:22:42 -0700

    add test

--- 27. power_of_four.py
+++ 27. power_of_four.py
@@ -16,3 +16,22 @@ def power_of_four(input_number):
       return False
 
   return True
+
+#Tests
+def power_of_four_test():
+
+  input_num1 = 16
+  input_num2 = 0
+  input_num3 = 5
+  input_num4 = 1
+  input_num5 = -4
+  
+  expected_output1 = True
+  expected_output2 = False
+  expected_output3 = False
+  expected_output4 = True
+  expected_output5 = False
+
+  return ( expected_output1 == power_of_four(input_num1), expected_output2 == power_of_four(input_num2), expected_output3 == power_of_four(input_num3), expected_output4 == power_of_four(input_num4), expected_output5 == power_of_four(input_num5) )
+
+print(power_of_four_test())
