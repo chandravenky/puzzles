be325e48ce2f514a2941cad2293ec96037825296
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-16 09:22:52 -0700

    add in class

--- 27. power_of_four.py
+++ 27. power_of_four.py
@@ -35,3 +35,26 @@ def power_of_four_test():
   return ( expected_output1 == power_of_four(input_num1), expected_output2 == power_of_four(input_num2), expected_output3 == power_of_four(input_num3), expected_output4 == power_of_four(input_num4), expected_output5 == power_of_four(input_num5) )
 
 print(power_of_four_test())
+
+#Leetcode
+class Solution(object):
+  def isPowerOfFour(self, input_number):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    if input_number==1 or input_number==4:
+      return True
+
+    if input_number<0 or input_number<4:
+      return False
+
+    while input_number>4:
+
+      input_number = float(input_number)/float(4)
+      remainder = input_number % 4
+
+      if remainder>0:
+        return False
+
+    return True
