0846ed0e3a63fbb24c2d3816c1329caee32e396a
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-17 08:30:52 -0700

    add in class

--- 28. reverse_string.py
+++ 28. reverse_string.py
@@ -26,3 +26,23 @@ def reverse_string_test():
   return ( expected_output1 == reverse_string(input_string_array1), expected_output2 == reverse_string(input_string_array2) )
 
 print(reverse_string_test())
+
+#Leetcode
+class Solution(object):
+  def reverseString(self, input_string_array):
+    """
+    :type s: List[str]
+    :rtype: None Do not return anything, modify s in-place instead.
+    """
+    traverse_length = len(input_string_array)//2
+
+    for i in range(0, traverse_length):
+
+      holding_pointer = input_string_array[i]
+
+      input_string_array[i] = input_string_array[len(input_string_array) -1 - i]
+
+      input_string_array[len(input_string_array) -1 - i] = holding_pointer
+
+    return input_string_array
+
