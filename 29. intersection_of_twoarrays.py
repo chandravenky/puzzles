f3d712f28dc994ca62ce97f4aea90a9f2e94c8e8
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-18 08:38:57 -0700

    in class

--- 29. intersection_of_twoarrays.py
+++ 29. intersection_of_twoarrays.py
@@ -31,3 +31,30 @@ def intersection_test():
   return ( expected_output1 == intersection(nums11, nums12), expected_output2 == intersection(nums21, nums22) )
 
 print(intersection_test())
+
+#Leetcode
+class Solution(object):
+  def intersection(self, nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+    store = {}
+    result_list = []
+
+    for i in range(0, len(nums1)):
+
+      if nums1[i] not in store:
+        store[nums1[i]] = 1
+
+
+    for i in range(0, len(nums2)):
+
+      if nums2[i] in store:
+        del store[nums2[i]]
+        result_list.append(nums2[i])
+
+
+    return result_list
+
