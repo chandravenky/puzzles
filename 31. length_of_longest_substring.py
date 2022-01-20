75de2cbb36b9e0a10b3d84c928193471bef07635
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-20 07:58:09 -0700

    add test

--- 31. length_of_longest_substring.py
+++ 31. length_of_longest_substring.py
@@ -28,3 +28,37 @@ def length_of_longest_sub(input_string):
         longest_length = second_slider - first_slider    
 
   return longest_length
+
+#Tests
+def length_of_longest_sub_test():
+
+  input_string1 = "bbbbb"
+  input_string2 = "abcabcbb"
+  input_string3 = "pwwkew"
+  input_string4 = " "
+  input_string5 = "   "
+  input_string6 = ""
+  input_string7 = ""
+  input_string8 = "aab"
+  input_string9 = "dvdf"
+  input_string10 = "ckilbkd"
+  input_string11 = "ckilbkdp"
+  input_string12 = "abba"
+  
+  expected_output1 = 1
+  expected_output2 = 3
+  expected_output3 = 3
+  expected_output4 = 1
+  expected_output5 = 1
+  expected_output6 = 0
+  expected_output7 = 1
+  expected_output8 = 2
+  expected_output9 = 3
+  expected_output10 =5
+  expected_output11 =6
+  expected_output12 = 2
+
+
+  return ( expected_output1 == length_of_longest_sub(input_string1), expected_output2 == length_of_longest_sub(input_string2), expected_output3 == length_of_longest_sub(input_string3), expected_output4 == length_of_longest_sub(input_string4), expected_output5 == length_of_longest_sub(input_string5), expected_output6 == length_of_longest_sub(input_string6), expected_output7 == length_of_longest_sub(input_string7), expected_output8 == length_of_longest_sub(input_string8), expected_output9 == length_of_longest_sub(input_string9), expected_output10 == length_of_longest_sub(input_string10), expected_output11 == length_of_longest_sub(input_string11), expected_output12 == length_of_longest_sub(input_string12) )
+
+print(length_of_longest_sub_test())
