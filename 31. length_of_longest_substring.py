b596e211ed454a9564955fb7d551137b50a5a0cd
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-20 07:58:22 -0700

    soln

--- 31. length_of_longest_substring.py
+++ 31. length_of_longest_substring.py
@@ -62,3 +62,52 @@ def length_of_longest_sub_test():
   return ( expected_output1 == length_of_longest_sub(input_string1), expected_output2 == length_of_longest_sub(input_string2), expected_output3 == length_of_longest_sub(input_string3), expected_output4 == length_of_longest_sub(input_string4), expected_output5 == length_of_longest_sub(input_string5), expected_output6 == length_of_longest_sub(input_string6), expected_output7 == length_of_longest_sub(input_string7), expected_output8 == length_of_longest_sub(input_string8), expected_output9 == length_of_longest_sub(input_string9), expected_output10 == length_of_longest_sub(input_string10), expected_output11 == length_of_longest_sub(input_string11), expected_output12 == length_of_longest_sub(input_string12) )
 
 print(length_of_longest_sub_test())
+
+#Leetcode
+class Solution(object):
+  def lengthOfLongestSubstring(self, input_string):
+    """
+    :type s: str
+    :rtype: int
+    """
+    tracker = {}
+    first_slider=0
+    second_slider=0
+    longest_length=0
+
+    if input_string =="":
+      return 0
+
+    if len(input_string.strip()) == 0:
+      return 1
+
+    if len(input_string) ==1:
+      return 1
+
+    for i in range(0, len(input_string)):
+
+      second_slider =i +1
+
+      if input_string[i] not in tracker:
+
+        tracker[input_string[i]]=i
+
+        if second_slider - first_slider > longest_length:
+
+          longest_length = second_slider - first_slider
+
+      else:
+
+        first_slider = max(tracker[input_string[i]] + 1, first_slider)
+
+        tracker[input_string[i]]  = i
+
+        if second_slider - first_slider > longest_length:
+
+          longest_length = second_slider - first_slider    
+
+    return longest_length
+
+
+
+
