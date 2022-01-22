7b5146318d94c124bdf6de4d5fecf60d501ab61a
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-22 09:01:48 -0700

    add tests

--- 33. reverse_interger.py
+++ 33. reverse_interger.py
@@ -38,3 +38,28 @@ def reverse_integer2(x):
     return 0
 
   return output
+
+#Tests
+def reverse_integer_test():
+
+  input_num1 = 123
+  input_num2 = -123
+  input_num3 = 120
+  input_num4 = 9463847412
+  input_num5 = 2563847412
+  input_num6 = -2563847412
+
+  expected_output1 = 321
+  expected_output2 = -321
+  expected_output3 = 21
+  expected_output4 = 2147483649
+  expected_output5 = 0
+  expected_output6 = 0
+
+  return ( expected_output1 == reverse_integer(input_num1), expected_output2 == reverse_integer(input_num2), expected_output3 == reverse_integer(input_num3), expected_output4 == reverse_integer(input_num4), expected_output5 == reverse_integer(input_num5),
+  expected_output6 == reverse_integer(input_num6), 
+
+  expected_output1 == reverse_integer(input_num1), expected_output2 == reverse_integer(input_num2), expected_output3 == reverse_integer(input_num3), expected_output4 == reverse_integer(input_num4), expected_output5 == reverse_integer(input_num5),
+  expected_output6 == reverse_integer(input_num6) )
+
+print(reverse_integer_test())
