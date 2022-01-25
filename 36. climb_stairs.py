0345d4ae3791b4ea826bca1aa6f005a6a2404a44
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-25 08:32:33 -0700

    add tests

--- 36. climb_stairs.py
+++ 36. climb_stairs.py
@@ -10,3 +10,22 @@ def climb_stairs(n):
     ways.append(ways[-1] + ways[-2])
 
   return ways[-1]
+
+#Tests
+def climb_stairs_test():
+
+  input_num1= 2
+  input_num2=3
+  input_num3= 4
+  input_num4= 5
+
+  expected_output1= 2
+  expected_output2= 3
+  expected_output3= 5
+  expected_output4= 8
+
+  result_tuple = ( expected_output1 == climb_stairs(input_num1), expected_output2 == climb_stairs(input_num2), expected_output3 == climb_stairs(input_num3), expected_output4 == climb_stairs(input_num4) )
+
+  return result_tuple
+
+print(climb_stairs_test())
