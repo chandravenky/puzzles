0bf996acc7e0a5d0348880fc41d8f70f796c4e88
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-26 07:51:28 -0700

    using dp in class

--- 37. house_robber.py
+++ 37. house_robber.py
@@ -30,3 +30,24 @@ def rob_house_test():
   return result_tuple
 
 print(rob_house_test())
+
+#Leetcode
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)<3:
+            
+            return max(nums)
+            
+        cache = [nums[0], max(nums[0], nums[1])]
+
+        for i in range(2, len(nums)):
+
+            new_rob_amount = cache[-2] + nums[i]
+
+            cache.append(max(new_rob_amount, cache[-1]))
+
+        return cache[-1]
