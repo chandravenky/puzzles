167d967a002ca47b1c7b49104796e607379d04eb
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-26 07:51:18 -0700

    add tests

--- 37. house_robber.py
+++ 37. house_robber.py
@@ -13,3 +13,20 @@ def rob_house(nums):
     cache.append(max(new_rob_amount, cache[-1]))
 
   return cache[-1]
+
+#Tests
+def rob_house_test():
+
+  input_array1= [1,2,3,1]
+  input_array2= [2,7,9,3,1]
+  input_array3= [2,1,1,2]
+
+  expected_output1= 4
+  expected_output2= 12
+  expected_output3= 4
+
+  result_tuple = ( expected_output1 == rob_house(input_array1), expected_output2 == rob_house(input_array2), expected_output3 == rob_house(input_array3) )
+
+  return result_tuple
+
+print(rob_house_test())
