c5c1802b4db958b5a09240a7483bbd5938922ba5
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-27 07:38:38 -0700

    add class with dp

--- 38. pascal_triangle.py
+++ 38. pascal_triangle.py
@@ -56,3 +56,38 @@ def pascal_triangle_test():
   return actual_output1== expected_output1, actual_output2== expected_output2, actual_output3== expected_output3, actual_output4== expected_output4, actual_output5== expected_output5, actual_output6== expected_output6, actual_output7== expected_output7
 
 print(pascal_triangle_test())
+
+#Leetcode
+class Solution(object):
+    def generate(self, n):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result_list = [[1], [1,1], [1,2,1]]
+        
+        
+        if n<3:
+            return result_list[:n]
+
+        for i in range(3, n):
+
+            num_elements_in_pattern = (i)//2
+
+            calculated_list = []
+
+            for j in range(1, num_elements_in_pattern+1):
+
+                calculated_list.append(result_list[-1][j] + result_list[-1][j-1])
+
+
+            if i%2!=0:
+                print("yes")
+                current_row_res = [1] + calculated_list + calculated_list[::-1] + [1]
+
+            else:
+                current_row_res = [1] + calculated_list + calculated_list[:len(calculated_list)-1][::-1] + [1]
+
+            result_list.append(current_row_res)
+
+        return result_list
