23c42a4bc4aac38366424712fb4f24681c8ae9e3
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-28 07:33:01 -0700

    add test

--- 39. search_in_rotated_array.py
+++ 39. search_in_rotated_array.py
@@ -36,3 +36,27 @@ def search(nums, target):
         end_pointer = current_pointer -1
         
   return -1
+
+def search_test():
+
+  input_array1, input_target1 = [4,5,6,7,0,1,2], 0
+  input_array2, input_target2 = [4,5,6,7,0,1,2], 3
+  input_array3, input_target3 = [1], 0
+  input_array4, input_target4 = [1], 1
+  input_array5, input_target5 = [2,0], 0
+  input_array6, input_target6 = [1,3], 1
+  input_array7, input_target7 = [3,5,1], 3
+
+  expected_output1= 4
+  expected_output2= -1
+  expected_output3= -1
+  expected_output4= 0
+  expected_output5= 1
+  expected_output6= 0
+  expected_output7= 0
+
+  result_tuple = ( expected_output1 == search(input_array1, input_target1), expected_output2 == search(input_array2, input_target2), expected_output3 == search(input_array3, input_target3), expected_output4 == search(input_array4, input_target4), expected_output5 == search(input_array5, input_target5), expected_output6 == search(input_array6, input_target6), expected_output7 == search(input_array7, input_target7) )
+
+  return result_tuple
+
+print(search_test())
