6e2e7909db4bb86623fa434716b05cc425fb8190
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-28 07:33:13 -0700

    add in class

--- 39. search_in_rotated_array.py
+++ 39. search_in_rotated_array.py
@@ -60,3 +60,48 @@ def search_test():
   return result_tuple
 
 print(search_test())
+
+#Leetcode
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        start_pointer = 0
+        end_pointer = len(nums)-1
+
+        while start_pointer <= end_pointer:
+
+            current_pointer = (start_pointer + end_pointer)//2
+
+            if nums[current_pointer] == target:
+                return current_pointer
+
+            #if left hand side is strictly increasing
+            elif nums[current_pointer] - nums[start_pointer]>=0:
+
+              #if target is in left hand side
+              if nums[start_pointer] <= target and target <= nums[current_pointer]:
+                end_pointer = current_pointer-1
+
+              #if target is in right hand side
+              else:
+                start_pointer = current_pointer + 1
+
+            #if left handside is not strictly increasing, then right side is
+            else: 
+
+              #if target is in right hand side
+              if target <= nums[end_pointer] and target >= nums[current_pointer]:
+
+                start_pointer = current_pointer + 1
+
+              #if target is in left hand side
+              else:
+
+                end_pointer = current_pointer -1
+
+        return -1
