6a490a289f8e7b6f5102a2dd102e25d032f0e947
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-29 08:04:08 -0700

    2 out of 3

---
+++ 40. two_out_of_three.py
@@ -0,0 +1,42 @@
+#Solution
+def two_out_of_three(nums1, nums2, nums3):
+
+  stored_master = {}
+  stored_1 = {}
+  stored_2 = {}
+  stored_3 = {}
+
+  for i in range(0, len(nums1)):
+    
+    if nums1[i] not in stored_1:
+      stored_1[nums1[i]] = 1
+      stored_master[nums1[i]] = 1
+
+    else:
+      pass
+  
+  for i in range(0, len(nums2)):
+    
+    if nums2[i] not in stored_master:
+      stored_2[nums2[i]] = 1
+      stored_master[nums2[i]] = 1
+
+    else:
+      if nums2[i] not in stored_2: 
+        stored_master[nums2[i]] = 2
+  
+  for i in range(0, len(nums3)):
+    
+    if nums3[i] not in stored_master:
+      stored_3[nums3[i]] = 1
+      stored_master[nums3[i]] = 1
+
+    else:
+      if nums3[i] not in stored_3:
+        stored_master[nums3[i]] = 2
+  
+
+  final_list = { key: value for key, value in stored_master.items() if value>1 }
+      
+  return list(final_list.keys())
+
