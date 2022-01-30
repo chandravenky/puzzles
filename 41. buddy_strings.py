67f985056b99e1251c250256277eee2f2ad64a9b
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-30 09:32:26 -0700

    add in class

--- 41. buddy_strings.py
+++ 41. buddy_strings.py
@@ -65,3 +65,57 @@ def buddy_strings_test():
   return result_tuple
 
 print(buddy_strings_test())
+
+#Leetcode
+class Solution(object):
+    def buddyStrings(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+
+        unmatch = {}
+        match={}
+
+        for i in range(0, len(s)):
+
+            if s[i]!= goal[i]:
+                unmatch[s[i]] = i
+
+            else:
+                match[s[i]] = 1 if s[i] not in match else match[s[i]] + 1
+
+            if len(unmatch)>2:
+                return False
+
+        same_key = {key:value for key, value in match.items() if value>=2}
+        
+        if len(unmatch)==1:
+            return False
+        
+        if len(unmatch)<2 and len(same_key)==0:
+            return False
+
+        elif len(unmatch)==0 and len(same_key)>0:
+            return True
+
+        else:
+
+            #replace in s
+            letters = [letter for letter in unmatch.keys()]
+            indices = [index for index in unmatch.values()]
+
+            #swap
+            s_new = list(s)
+            s_new[indices[0]] = letters[1]
+            s_new[indices[1]] = letters[0]
+            s_new = ''.join(s_new)
+
+            if s_new == goal:
+                return True
+
+            return False
+        
