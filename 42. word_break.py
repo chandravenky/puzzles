71d0ffb17fbf14222d419284718e018460b7c228
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-01-31 08:07:19 -0700

    add tests

--- 42. word_break.py
+++ 42. word_break.py
@@ -24,3 +24,27 @@ def word_break2(s, word_dict):
       return True
   
   return False
+
+#12:30 a working solution by 12:45
+#Tests
+def word_break2_test():
+
+  input_s1 = "leetcode"
+  input_s2 = "applepenapple"
+  input_s3 = "catsandog"
+
+  input_word_dict1 = ["leet","code"]
+  input_word_dict2 = ["apple","pen"]
+  input_word_dict3 = ["cats","dog","sand","and","cat"]
+
+  expected_output1 = True
+  expected_output2 = True
+  expected_output3 = False
+
+  actual_output1 = word_break2(input_s1, input_word_dict1)
+  actual_output2 = word_break2(input_s2, input_word_dict2)
+  actual_output3 = word_break2(input_s3, input_word_dict3)
+
+  return ( expected_output1 == actual_output1, expected_output2 == actual_output2, expected_output3 == actual_output3 )
+
+print(word_break2_test())
