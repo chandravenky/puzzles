bb1896b636501237c2272873f70c5cb317e4f196
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-02 08:49:05 -0700

    add test

--- 44. third_max.py
+++ 44. third_max.py
@@ -1,3 +1,4 @@
+# Solution
 def third_max(nums):
 
   first_max = -2147483649
@@ -27,3 +28,37 @@ def third_max(nums):
     third_max = first_max
 
   return third_max
+
+
+
+#Tests
+def third_max_test():
+
+
+  input_array1 =  [1,2,3,4,5,4,3,2,1]
+  input_array2 =  [3,2,1]
+  input_array3 =  [2,2,3,1]
+  input_array4 =  [1,2,2,5,3,5]
+  input_array5 =  [1,2,-2147483648]
+  input_array6 =  [1,2]
+  input_array7 =  [2,2,2,2,2,2,2]
+
+  expected_output1 = 3
+  expected_output2 = 1
+  expected_output3 = 1
+  expected_output4 = 2
+  expected_output5 = -2147483648
+  expected_output6 = 2
+  expected_output7 = 2
+
+  actual_output1 = third_max(input_array1)
+  actual_output2 = third_max(input_array2)
+  actual_output3 = third_max(input_array3)
+  actual_output4 = third_max(input_array4)
+  actual_output5 = third_max(input_array5)
+  actual_output6 = third_max(input_array6)
+  actual_output7 = third_max(input_array7)
+
+  return ( expected_output1 == actual_output1, expected_output2 == actual_output2, expected_output3 == actual_output3, expected_output4 == actual_output4, expected_output5 == actual_output5, expected_output6 == actual_output6, expected_output7 == actual_output7 )
+
+print(third_max_test())
