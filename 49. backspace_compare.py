8be19b65ec25542531b79dd778c66baa97ef7eab
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-08 07:43:17 -0700

    final in class

--- 49. backspace_compare.py
+++ 49. backspace_compare.py
@@ -51,3 +51,46 @@ def backspace_compare_test():
   return result_tuple
 
 print(backspace_compare_test())
+
+#Leetcode
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        stack_a = []
+        stack_b = []
+
+        for i in range(0, len(s)):
+
+            if s[i] == '#':
+
+                if len(stack_a) ==0:
+                    pass
+                else:
+                    stack_a.pop()
+
+            else:
+                stack_a.append(s[i])
+
+        for j in range(0, len(t)):
+
+            if t[j] == '#':
+
+                if len(stack_b) ==0:
+                    pass
+                else:
+                    stack_b.pop()
+
+            else:
+                stack_b.append(t[j])
+
+        #string output of stack_a
+        stack_a_string = ''.join(stack_a)
+
+        #string output of stack_b
+        stack_b_string = ''.join(stack_b)
+
+        return stack_a_string == stack_b_string
