448ec6eb0be73a71a1f10f83b17c78a8ed6ab9a4
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-08 07:43:08 -0700

    add test

--- 49. backspace_compare.py
+++ 49. backspace_compare.py
@@ -35,3 +35,19 @@ def backspace_compare(s,t):
   stack_b_string = ''.join(stack_b)
   
   return stack_a_string == stack_b_string
+
+#Tests
+def backspace_compare_test():
+
+  input_string_1a, input_string_1b = "ab#c", "ad#c"
+  input_string_2a, input_string_2b = "ab#c", "ad#c"
+  input_string_3a, input_string_3b = "a#c", "b"
+  input_string_4a, input_string_4b = "##aa", "##aa"
+
+  result_tuple = ( backspace_compare(input_string_1a, input_string_1b) == True, backspace_compare(input_string_2a, input_string_2b) == True, backspace_compare(input_string_3a, input_string_3b) == False,
+  backspace_compare(input_string_4a, input_string_4b) == True,
+   )
+
+  return result_tuple
+
+print(backspace_compare_test())
