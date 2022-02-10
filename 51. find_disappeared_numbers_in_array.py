c01911b58af7aa6a22531c9d1575bd25603626aa
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-10 08:00:38 -0700

    solution

--- 51. find_disappeared_numbers_in_array.py
+++ 51. find_disappeared_numbers_in_array.py
@@ -38,3 +38,30 @@ def find_disappeared_numbers_test():
 
 
 print(find_disappeared_numbers_test())
+
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        #8:19am
+        result1 = [0]*len(nums)
+        result2 = []
+
+        for i in range(0, len(nums)):
+
+            if result1[nums[i]-1]==0:
+                result1[nums[i]-1] = nums[i]
+
+            else:
+                pass
+
+        for i in range(0, len(result1)):
+
+            if i +1 != result1[i]:
+                result2.append(i+1)
+
+
+
+        return result2
