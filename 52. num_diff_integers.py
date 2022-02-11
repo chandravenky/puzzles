7e7e4755a6701db42fbc327135bdf9515c3ff58b
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-11 08:14:33 -0700

    add tests

--- 52. num_diff_integers.py
+++ 52. num_diff_integers.py
@@ -69,3 +69,27 @@ def num_different_integers2(s):
 
     nums = [int(item) for item in store.keys()]
     return len(set(nums))
+
+def num_different_integers_test():
+
+  input_string1 = "a123bc34d8ef34"
+  input_string2 = "leet1234code234"
+  input_string3 = "a1b01c001"
+  input_string4 = "gi851a851q8510v"
+  input_string5 = "0a0"
+  input_string6 = "a0"
+  input_string7 = "0a"
+  input_string8 = "0"
+
+  expected_output1 = 3
+  expected_output2 = 2
+  expected_output3 = 1
+  expected_output4 = 2
+  expected_output5 = 1
+  expected_output6 = 1
+  expected_output7 = 1
+  expected_output8 = 1
+
+  return ( expected_output1 == num_different_integers(input_string1), expected_output2 == num_different_integers(input_string2), expected_output3 == num_different_integers(input_string3), expected_output4 == num_different_integers(input_string4), expected_output5 == num_different_integers(input_string5), expected_output6 == num_different_integers(input_string6), expected_output7 == num_different_integers(input_string7), expected_output8 == num_different_integers(input_string8) )
+
+print(num_different_integers_test())
