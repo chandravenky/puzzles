a3c4c83505d5bac82384b67d976426c7c287e714
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-11 08:14:43 -0700

    final in class

--- 52. num_diff_integers.py
+++ 52. num_diff_integers.py
@@ -93,3 +93,46 @@ def num_different_integers_test():
   return ( expected_output1 == num_different_integers(input_string1), expected_output2 == num_different_integers(input_string2), expected_output3 == num_different_integers(input_string3), expected_output4 == num_different_integers(input_string4), expected_output5 == num_different_integers(input_string5), expected_output6 == num_different_integers(input_string6), expected_output7 == num_different_integers(input_string7), expected_output8 == num_different_integers(input_string8) )
 
 print(num_different_integers_test())
+
+class Solution(object):
+    def numDifferentIntegers(self, s):
+        """
+        :type word: str
+        :rtype: int
+        """
+        s = s + "x"
+        num_list = list("0123456789")
+        str_current = ""
+        store = {}
+
+        for i in range(0, len(s)):
+
+            if s[i] not in num_list:
+                if len(str_current)>0:
+                    store[str_current] = 1
+
+                    str_current = ""
+
+                else:
+                    pass
+
+            else:
+                if s[i] != "0":
+                    str_current = str_current + s[i]
+
+                else:
+                    if len(str_current)>0:
+                        str_current = str_current + s[i]
+
+                    else:
+                        if i+1<len(s):
+                            if s[i+1] not in num_list:
+                                str_current = str_current + s[i]
+
+                        else:
+                            pass
+
+        if len(str_current)>0:
+            store[str_current] = 1
+
+        return len(store.keys())
