74a5ce1e944a0f25ae0f8a5743ccb104f6436d75
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-14 07:02:35 -0700

    add tests

--- 56. arrange_coins.py
+++ 56. arrange_coins.py
@@ -20,3 +20,23 @@ def arrange_coins(n):
         return mid
         
     return end
+
+
+# #Tests
+def arrange_coins_test():
+
+  input_num1  = 5
+  input_num2  = 8
+  input_num3  = 1
+  input_num4  = 2
+  input_num5  = 3
+
+  expected_result1 = 2
+  expected_result2 = 3
+  expected_result3 = 1
+  expected_result4 = 1
+  expected_result5 = 2
+
+  return arrange_coins(input_num1) == expected_result1, arrange_coins(input_num2) == expected_result2, arrange_coins(input_num3) == expected_result3, arrange_coins(input_num4) == expected_result4, arrange_coins(input_num5) == expected_result5
+
+print(arrange_coins_test())
