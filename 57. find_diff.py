70410aa5ab2b430a41ee5a4d2d97a1eba871129a
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-15 07:57:52 -0700

    final solution

--- 57. find_diff.py
+++ 57. find_diff.py
@@ -43,3 +43,39 @@ def find_difference_test():
   return expected_output_1 == find_difference(input_s1, input_t1), expected_output_2 == find_difference(input_s2, input_t2), expected_output_3 == find_difference(input_s3, input_t3)
 
 print(find_difference_test())
+
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        
+        store_1 = list(s)
+        store_2 = list(t)
+
+        master_store = {}
+
+
+        for i in range(0, len(store_1)):
+
+            if store_1[i] not in master_store:
+                master_store[store_1[i]] = 1
+
+            else:
+                master_store[store_1[i]] = master_store[store_1[i]] + 1
+
+        for i in range(0, len(store_2)):
+
+            if store_2[i] not in master_store:
+                return store_2[i]
+
+            else:
+                master_store[store_2[i]] = master_store[store_2[i]] - 1
+
+                if master_store[store_2[i]] == 0:
+
+                    del master_store[store_2[i]]
+                    
+        return master_store.keys()[0]
