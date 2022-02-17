ee80a6fffbeaefab7cc2e19fe8925d16b5083a0c
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-17 07:32:36 -0700

    submitted

--- 59. word_pattern.py
+++ 59. word_pattern.py
@@ -46,3 +46,38 @@ def word_pattern_test():
   return ( expected_output1 == word_pattern(input_pattern1, input_s1), expected_output2 == word_pattern(input_pattern2, input_s2), expected_output3 == word_pattern(input_pattern3, input_s3), expected_output4 == word_pattern(input_pattern4, input_s4) )
 
 print(word_pattern_test())
+
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        
+        
+        patterns_list = list(pattern)
+        s_list =  s.split(" ")
+        
+        if len(patterns_list)!=len(s_list):
+            return False
+        
+        store = {}
+        for i in range(0, len(patterns_list)):
+            
+            if pattern[i] not in store:
+                store[pattern[i]] = s_list[i]
+
+                
+            else:
+                if store[pattern[i]] == s_list[i]:
+                    pass
+                else:
+                    return False
+                
+        if len(store) != len(set(store.values())):
+            return False
+        
+        return True
+            
+        
