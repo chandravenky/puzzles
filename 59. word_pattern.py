1425287e14e4bf2af275559b74252c380b0ef005
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-17 07:32:27 -0700

    add tests

--- 59. word_pattern.py
+++ 59. word_pattern.py
@@ -24,3 +24,25 @@ def word_pattern(pattern, s):
     return False
   
   return True
+
+#Test
+def word_pattern_test():
+
+  input_pattern1 = "abba"
+  input_pattern2 = "abba"
+  input_pattern3 = "aaaa"
+  input_pattern4 = "abba"
+
+  input_s1 = "dog cat cat dog"
+  input_s2 = "dog cat cat fish"
+  input_s3 = "dog cat cat dog"
+  input_s4 = "dog dog dog dog"
+
+  expected_output1 = True
+  expected_output2 = False
+  expected_output3 = False
+  expected_output4 = False
+
+  return ( expected_output1 == word_pattern(input_pattern1, input_s1), expected_output2 == word_pattern(input_pattern2, input_s2), expected_output3 == word_pattern(input_pattern3, input_s3), expected_output4 == word_pattern(input_pattern4, input_s4) )
+
+print(word_pattern_test())
