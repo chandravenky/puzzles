5b08609ec2f52e92effdf3679e5935ac128fee7b
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-18 07:29:39 -0700

    add tests

--- 60. license_key_formatting.py
+++ 60. license_key_formatting.py
@@ -1,3 +1,5 @@
+
+
 def license_key_formatting(s, k):
 
   s = s.upper()
@@ -12,3 +14,19 @@ def license_key_formatting(s, k):
 
   result = output[::-1]
   return result[1:]
+
+#Tests
+def license_key_formatting_test():
+
+  input_license_key1 = "5F3Z-2e-9-w"
+  input_license_key2 = "2-5g-3-J"
+
+  input_k1 = 4
+  input_k2 = 2
+
+  expected_output1 = "5F3Z-2E9W"
+  expected_output2 = "2-5G-3J"
+
+  return ( expected_output1 == license_key_formatting(input_license_key1, input_k1), expected_output2 == license_key_formatting(input_license_key2, input_k2) )
+
+print(license_key_formatting_test())
