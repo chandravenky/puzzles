44f4b179691d6e8b143c024f6f16850b9e491eae
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-19 07:07:43 -0700

    add tests

--- 61. Valid Palindrome II.py
+++ 61. Valid Palindrome II.py
@@ -9,3 +9,18 @@ def valid_palindrome(s):
       return True
   
   return False
+
+#Tests
+def valid_palindrome_test():
+
+  input_s1 = "aba"
+  input_s2 = "abca"
+  input_s3 = "abc"
+
+  expected_output1 = True
+  expected_output2 = True
+  expected_output3 = False
+
+  return ( expected_output1 == valid_palindrome(input_s1), expected_output2 == valid_palindrome(input_s2), expected_output3 == valid_palindrome(input_s3) )
+
+print(valid_palindrome_test())
