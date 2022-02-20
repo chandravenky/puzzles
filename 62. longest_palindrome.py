5c09e6c4f44f1fef3f3e24558a1df54df58bc817
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-20 09:44:38 -0700

    final submitted

--- 62. longest_palindrome.py
+++ 62. longest_palindrome.py
@@ -39,3 +39,34 @@ def longest_palindrome_test():
   return ( expected_output1 == longest_palindrome(input_s1), expected_output2 == longest_palindrome(input_s2), expected_output3 == longest_palindrome(input_s3) )
 
 print(longest_palindrome_test())
+
+#Solution
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        store = {}
+        
+        for i in range(0, len(s)):
+            
+            if s[i] not in store:
+                store[s[i]] = 1
+            
+            else:
+                store[s[i]] = store[s[i]] + 1
+                
+        pair_characters_store = {key: value for key, value in store.items() if value>=2}
+        
+        len_palindrome_pair_list = [int(value/2) for value in pair_characters_store.values()]
+        
+        extra_char_in_pair = [value for value in pair_characters_store.values() if value%2!=0]
+        
+        tot_character = sum(len_palindrome_pair_list)
+        
+        extra_char = 1 if len(store) > len(pair_characters_store) or len(extra_char_in_pair)>0 else 0
+        
+        return tot_character*2 + extra_char
+        
+        
