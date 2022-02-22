47337e735d13207295219cd5ac44d1ee366e7a82
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-22 07:28:56 -0700

    final soln

--- 64. Max Consecutive Ones.py
+++ 64. Max Consecutive Ones.py
@@ -46,3 +46,37 @@ def max_consecutive_ones_test():
   return ( expected_output1 == max_consecutive_ones(input_nums1), expected_output2 == max_consecutive_ones(input_nums2), expected_output3 == max_consecutive_ones(input_nums3), expected_output4 == max_consecutive_ones(input_nums4) )
 
 print(max_consecutive_ones_test())
+
+#Submit
+class Solution(object):
+  def findMaxConsecutiveOnes(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    max_count =0
+    count = 0
+    prev = 0
+
+    if nums == [1]:
+      return 1
+
+    for i in range(0, len(nums)):
+      if nums[i]==1:
+
+        if prev==1:
+          count=count+1
+
+        else:
+          count = 1
+        prev = 1
+
+      else:
+        prev= 0 
+
+      if count> max_count:
+        max_count = count
+
+    return max_count
+
+
