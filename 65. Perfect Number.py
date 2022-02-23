79f7c37396b39aec0e14d2db957b471412d8919b
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-23 07:31:25 -0700

    submitted

--- 65. Perfect Number.py
+++ 65. Perfect Number.py
@@ -33,3 +33,25 @@ def perfect_number_test():
   return ( expected_output1 == perfect_number(input_nums1), expected_output2 == perfect_number(input_nums2), expected_output3 == perfect_number(input_nums3), expected_output4 == perfect_number(input_nums4))
 
 print(perfect_number_test())
+
+#Submit
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        divisor_sum = 1
+        
+        if num == 1:
+            return False
+
+        for i in range(2, int(num**0.5) + 1):
+
+            if num%i == 0:
+                divisor_sum = divisor_sum + i + num/i
+
+        if divisor_sum == num:
+            return True
+
+        return False
