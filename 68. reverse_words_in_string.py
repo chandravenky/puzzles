03b7337e7132b5f00084080691561d24d77cca71
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-25 07:49:37 -0700

    add tests

--- 68. reverse_words_in_string.py
+++ 68. reverse_words_in_string.py
@@ -12,3 +12,18 @@ def reverse_words(s):
     words_list[len(words_list)-1-i] = hold_word
   
   return ' '.join(words_list)
+
+#Add tests
+def reverse_words_test():
+
+  input_s1 = "the sky is blue"
+  input_s2 = "  hello world  "
+  input_s3 = "a good   example"
+
+  expected_output1 = "blue is sky the"
+  expected_output2 = "world hello"
+  expected_output3 = "example good a"
+
+  return ( expected_output1 == reverse_words(input_s1), expected_output2 == reverse_words(input_s2), expected_output3 == reverse_words(input_s3) )
+
+print(reverse_words_test())
