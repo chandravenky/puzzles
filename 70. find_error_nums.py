94cb6aeac1c47c6b80b4cb05032dca35a5438485
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-27 07:37:35 -0700

    final soln

--- 70. find_error_nums.py
+++ 70. find_error_nums.py
@@ -28,3 +28,24 @@ def find_error_nums_test():
   return ( expected_output1 == find_error_nums(input_nums1), expected_output2 == find_error_nums(input_nums2) )
 
 print(find_error_nums_test())
+  
+#Final solution
+class Solution(object):
+    def findErrorNums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        store = {}
+        running_sum = 0
+        tot_expected_sum = len(nums)*(len(nums)+1)/2
+
+        for i in range(0, len(nums)):
+
+            if nums[i] not in store:
+                store[nums[i]] = 1
+                running_sum = running_sum + nums[i]
+            else:
+                num_occured_twice = nums[i]
+
+        return [num_occured_twice, tot_expected_sum - running_sum]
