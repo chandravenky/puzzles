8cf19f22fc5228f15dcd6e9e1089bbe602d2ba1f
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-02-28 06:57:01 -0700

    add tests

--- 71. can_place_flowers.py
+++ 71. can_place_flowers.py
@@ -21,3 +21,25 @@ def can_place_mismatch(flowerbed, n):
       i = i+2
           
   return n<=0
+
+#Add tests
+def can_place_mismatch_test():
+
+  input_flowerbed1 = [1,0,0,0,1]
+  input_flowerbed2 = [1,0,0,0,1]
+  input_flowerbed3 = [1,0,0,0,1,0,0]
+  input_flowerbed4 = [0,0,1,0,0]
+
+  input_n1 = 1
+  input_n2 = 2
+  input_n3 = 2
+  input_n4 = 1
+
+  expected_output1 = True
+  expected_output2 = False
+  expected_output3 = True
+  expected_output4 = True
+
+  return ( expected_output1 == can_place_mismatch(input_flowerbed1, input_n1), expected_output2 == can_place_mismatch(input_flowerbed2, input_n2), expected_output3 == can_place_mismatch(input_flowerbed3, input_n3), expected_output4 == can_place_mismatch(input_flowerbed4, input_n4) )
+
+print(can_place_mismatch_test())a
