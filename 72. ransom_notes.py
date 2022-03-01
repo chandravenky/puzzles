8a2175751cc5363a9d00c11f78d81b962bf2c62a
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-01 07:23:59 -0700

    final submitted

--- 72. ransom_notes.py
+++ 72. ransom_notes.py
@@ -43,3 +43,35 @@ def ransom_note_test():
   return ( expected_output1 == ransom_note(input_ransom1, input_magazine1), expected_output2 == ransom_note(input_ransom2, input_magazine2), expected_output3 == ransom_note(input_ransom3, input_magazine3) )
 
 print(ransom_note_test())
+
+#Solution
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        magazine_store = {}
+        
+        for i in range(0, len(magazine)):
+            
+            if magazine[i] not in magazine_store:
+                magazine_store[magazine[i]] = 1
+                
+            else:
+                magazine_store[magazine[i]] = magazine_store[magazine[i]] + 1
+                
+        for i in range(0, len(ransomNote)):
+            
+            if ransomNote[i] not in magazine_store:
+                return False
+            
+            else:
+                magazine_store[ransomNote[i]] = magazine_store[ransomNote[i]] - 1
+                
+                if magazine_store[ransomNote[i]] == 0:
+                    del magazine_store[ransomNote[i]]
+                    
+        return True
+        
