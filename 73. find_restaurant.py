3c4a0e55486927217e1b031edbda8b9ed669f4b6
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-02 07:58:08 -0700

    final soln

--- 73. find_restaurant.py
+++ 73. find_restaurant.py
@@ -43,3 +43,38 @@ def find_restaurant_test():
   return ( expected_output1 == find_restaurant(input_list1a, input_list1b), expected_output2 == find_restaurant(input_list2a, input_list2b), expected_output3 == find_restaurant(input_list3a, input_list3b) )
 
 print(find_restaurant_test())
+
+#Solution
+class Solution(object):
+    def findRestaurant(self, list1, list2):
+        """
+        :type list1: List[str]
+        :type list2: List[str]
+        :rtype: List[str]
+        """
+        
+        store = {}
+        min_sum = 99999999
+        result= []
+        
+        for i in range(0, len(list1)):
+            
+            store[list1[i]] = i
+            
+        for i in range(0, len(list2)):
+            
+            if list2[i] in store:
+                sum_total = store[list2[i]] + i
+                if sum_total == min_sum:
+                    result.append(list2[i])
+                    
+                if sum_total< min_sum:
+                    min_sum = sum_total
+                    result = [list2[i]]
+                    
+                    
+        return result
+            
+        
+        
+        
