007962871eb933c61dceb81b4ad8b25242fa2078
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-04 07:50:46 -0700

    add tests

--- 75. robot_return_to_origin.py
+++ 75. robot_return_to_origin.py
@@ -17,3 +17,16 @@ def judge_circle(moves):
     init = [init[0] + shift[move][0], init[1] + shift[move][1]]
         
   return init == [0,0]
+
+#Add tests
+def judge_circle_test():
+
+  input_moves1 = "UD"
+  input_moves2 = "LL"
+
+  expected_output1 = True
+  expected_output2 = False
+
+  return ( expected_output1 == judge_circle(input_moves1), expected_output2 == judge_circle(input_moves2) )
+
+print(judge_circle_test())
