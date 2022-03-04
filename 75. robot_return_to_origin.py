2de3535d3bb2a74b19298373c848a3d161f170fd
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-04 07:50:55 -0700

    add soln

--- 75. robot_return_to_origin.py
+++ 75. robot_return_to_origin.py
@@ -30,3 +30,26 @@ def judge_circle_test():
   return ( expected_output1 == judge_circle(input_moves1), expected_output2 == judge_circle(input_moves2) )
 
 print(judge_circle_test())
+
+#Solution
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        shift = {
+                'U': [1,0],
+                'D': [-1,0],
+                'L': [0,-1],
+                'R': [0,1]
+                }
+        init = [0,0]
+        
+        moves_list = list(moves)
+        
+        for move in moves_list:
+            
+            init = [init[0] + shift[move][0], init[1] + shift[move][1]]
+            
+        return init == [0,0]
