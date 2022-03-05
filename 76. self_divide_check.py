321c09af2ab098df0a1070bd648d2776c54c3436
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-05 08:54:17 -0700

    add tests

--- 76. self_divide_check.py
+++ 76. self_divide_check.py
@@ -13,4 +13,20 @@ def self_divide_check(num):
     if divmod(num, num_int)[1] !=0:
       return False
       
-  return True    
+  return True        
+
+def self_dividing_numbers(left, right):
+    """
+    :type left: int
+    :type right: int
+    :rtype: List[int]
+    """
+    result=[]
+    
+    for i in range(left, right+1):
+        
+      if self_divide_check(i):
+          
+        result.append(i)
+            
+    return result
