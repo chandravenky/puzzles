1d665e018d917d147d895a8a2ddb48ecd744a273
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-15 08:03:16 -0600

    final soln

--- 86. generate_paranthesis.py
+++ 86. generate_paranthesis.py
@@ -36,3 +36,32 @@ def generate_parenthesis_test():
   return ( expected_output1 == generate_parenthesis(input_n1), expected_output2 == generate_parenthesis(input_n2) )
 
 print(generate_parenthesis_test())
+
+#Submit
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        stack = []
+        result = []
+        
+        def backtrack(open_n, closed_n):
+            if open_n == closed_n == n:
+                result.append(''.join(stack))
+                
+            if open_n<n:
+                stack.append("(")
+                backtrack(open_n+1, closed_n)
+                stack.pop()
+                
+            if closed_n < open_n:
+                stack.append(")")
+                backtrack(open_n, closed_n+1)
+                stack.pop()
+        
+        backtrack(0,0)
+        
+        return result
+        
