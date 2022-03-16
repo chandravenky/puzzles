bcedd16fc01a857a2c0494fdabd4cf36effc8618
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-16 07:41:52 -0600

    final soln

--- 87. permutations.py
+++ 87. permutations.py
@@ -34,3 +34,32 @@ def permute_test():
   return ( expected_output1 == permute(input_nums1), expected_output2 == permute(input_nums2), expected_output3 == permute(input_nums3) )
 
 print(permute_test())
+
+#Submission
+import copy
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        res = []
+        perm = []
+
+        def backtrack(i):
+            if i == len(nums):
+                res.append(copy.deepcopy(perm))
+                return
+
+            for num in nums:
+                if num not in perm:
+
+                    perm.append(num)
+                    backtrack(i + 1)
+                    perm.pop()
+
+        backtrack(0)
+
+        return res
+                
