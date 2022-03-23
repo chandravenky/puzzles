ec5194e935949ed85821fed80a1a2f8bb170da03
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-23 08:20:34 -0600

    combine test

--- 95. combinations.py
+++ 95. combinations.py
@@ -18,3 +18,21 @@ def combine(n, k):
   backtrack(1)
     
   return output
+
+
+def combine_test():
+
+  input_n1 = 4
+  input_n2 = 4
+
+  input_k1 = 2
+  input_k2 = 3
+
+  expected_output1 = [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+  expected_output2 = [[1,2,3],[1,2,4],[1,3,4],[2,3,4]]
+
+  result_tuple = ( expected_output1 == combine(input_n1, input_k1), expected_output2 == combine(input_n2, input_k2) )
+
+  return result_tuple == ( True, True )
+
+print(combine_test())
