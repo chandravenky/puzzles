bdeff90f28bf13f90e6243b528ccfab16e6b8370
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-23 08:20:43 -0600

    soln

--- 95. combinations.py
+++ 95. combinations.py
@@ -36,3 +36,25 @@ def combine_test():
   return result_tuple == ( True, True )
 
 print(combine_test())
+
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        def backtrack(first, curr=[]):
+            if len(curr)==k:
+                output.append(copy.deepcopy(curr))
+                return
+            for j in range(first, n+1):
+                curr.append(j)
+                backtrack(j+1, curr)
+                curr.pop()
+
+        output = []
+        backtrack(1)
+    
+        return output
+        
