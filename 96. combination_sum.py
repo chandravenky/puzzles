ceb06966beb4bfc2248b4d02fda7b9d1dce1b2e6
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-24 08:10:48 -0600

    add test

--- 96. combination_sum.py
+++ 96. combination_sum.py
@@ -20,3 +20,21 @@ def combination_sum(candidates, target):
     backtrack(i,[])
 
   return output
+
+
+
+def combination_sum_test():
+
+  input_candidates1 = [2,3,6,7]
+  input_candidates2 = [2,3,5]
+  input_candidates3 = [2]
+
+  input_target1 = 7
+  input_target2 = 8
+  input_target3 = 1
+
+  expected_output1 = [[2,2,3],[7]]
+  expected_output2 = [[2,2,2,2],[2,3,3],[3,5]]
+  expected_output3 = []
+
+  return ( expected_output1 == combination_sum(input_candidates1, input_target1), expected_output2 == combination_sum(input_candidates2, input_target2), expected_output3 == combination_sum(input_candidates3, input_target3) )
