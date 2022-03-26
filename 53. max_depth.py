d17bc5ecfab141f556d1c15e3d32afdae27e181c
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2022-03-26 08:45:49 -0600

    max_depth

---
+++ 53. max_depth.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        if root==None:
+            return 0
+        
+        if root.left==None and root.right == None:
+            return 1
+        
+        if root.left == None:
+            return 1 + self.maxDepth(root.right)
+        
+        if root.right == None:
+            return 1 + self.maxDepth(root.left)
+            
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
